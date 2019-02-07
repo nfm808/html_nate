8445c862fe26e24430fcb4df8888fe39c0fac13c
Nathan Fowler <luxurysandbox@gmail.com>
2019-02-07 12:14:35 -0500

    change of sun and added animation

--- index.js
+++ index.js
@@ -1,47 +1,90 @@
 'use strict'
 
-// retrieves the html to be added to DOM
-function getHtml() {
-  //   console.log(svg[0]["sailboat"]);
+//transition new DOM elements
+function createNewPage(string, num) {
+  console.log(`createNewPage() ran`);
+  let svg = DATA[`${num}`][`${string}`][1];
+  let nav = DATA[`${num}`][`${string}`][0];
+  
+
 };
-// adds the element to DOM
-// function addElement() {
-//   getElement('.sailboat')
-// };
-//transition out elements
-function transitionOut() {
-  let speed = 200
-  $('#sailboat', '#volcano').animate('left: 140%vw',speed)
+//welcome message animation
+function welcomeAnimation() {
+  let timer = setTimeout(e, 2000);
+  function e() {
+    $('.welcome').animate({fontSize: "4rem"}, 1000);
+    clearTimeout(timer);
+  };
+  let timer2 = setTimeout(f, 10000);
+  function f() {
+    $('.welcome').animate({fontSize: "5rem"}, 200);
+    clearTimeout(timer2);
+    fadeOut('welcome');
+    let timer3 = setTimeout(g, 100);
+    function g() {
+      $('.welcome').remove();
+      clearTimeout(timer3);
+    };
+  };
+};
+//nav grow in transition
+function navGrow() {
+  window.setTimeout(e, 16000)
+  function e() {
+    $(`li`).animate({fontSize: "4rem"}, 1000);
+  };
+}
+//nav fade out transition
+function navFadeOut() {
+  $(`#nav`).animate({opacity: 0}, 1100);
+};
+//font size increase
+function fontGrow(string) {
+    $(`#${string}`).animate({fontSize: "5.6rem"}, 800);
+    $(`#${string}`).animate({fontSize: "5rem"}, 1);
 };
 
-//button click
+//fade out background elements
+function fadeOut(string) {
+  $(`.${string}`).animate({opacity: 0}, 700);
+};
+//button click listener
 function buttonClick() {
-  $('button').click(function() {
-    return (this.id);
+  let current = $('body').data("id");
+  let target = "";
+  let index = 2;
+  $('.navbutton').click(function() {
+    target = (this.id);
+    fadeOut(current);
+    fontGrow(target);
+    navFadeOut();
+    current = target;
+    createNewPage(target, index);
   });
 };
 // svg click for audio
-function svgClick() {
-  let audio = new Audio('assets/audio/exp1.mp3')
-  $('.sun, #sun').click(function() {
+function audioClick() {
+  let audio = new Audio('assets/audio/sailboat.mp3')
+  $('.sun, #sun, #speaker').click(function() {
     audio.play();
   });
 };
 function handlePage() {
-  // checkString();
+  welcomeAnimation();
   buttonClick();
   svgClick();
-  // addElement();
+  navGrow();
   findPathLength();
-  console.log(findPathLength());
+  // addElement();
+  // findPathLength(volcano);
 };
 
 // find path length of svg
 function findPathLength() {
-  let path = document.querySelector(".sun path");
+  let path = document.querySelector(`.sun path`);
   let total_length = path.getTotalLength();
-  return Math.round(total_length);
-  
+  Math.round(total_length);
+  console.log(total_length);
 }
 
 
