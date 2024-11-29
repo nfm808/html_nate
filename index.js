804ba619ea080ef1fdc079a453cf8775583fa6e9
htmlnate <nathanfowlermusic@gmail.com>
2024-11-28 15:41:41 -1000

    Updated portfolio and about section.

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ function handleSubmitForm() {
       let formData = {
         access_key: "5c1f204f-8111-458b-b614-b15f735c3746",
         subject: "htmlnate form submission",
+        website: "htmlnate.com",
         name: e.target[0].value,
         email: e.target[1].value,
         message: e.target[2].value,
@@ -218,6 +219,146 @@ function fadeInBackground(string) {
     )}'`
   );
 }
+
+function renderPortfolioItems() {
+  const portfolioItems = [
+    {
+      img: "konasuncleaning.jpg",
+      heading: "Kona Sun Cleaning",
+      technology: "NextJS | Tailwind CSS",
+      body: "Created a performant NextJS site for a local window cleaning business and setup google ads.",
+      liveSite: "konasuncleaning.com",
+    },
+    {
+      img: "alottawoodfiredpizza.jpg",
+      heading: "Alotta Wood Fired Pizza",
+      technology: "NextJS | Tailwind CSS",
+      body: "Created a performant NextJS site for a wood fired pizza business in Portland, OR.",
+      liveSite: "alottawoodfiredpizza.com",
+    },
+    {
+      img: "amygreenwellgarden.jpg",
+      heading: "Amy Greenwell Ethnobotanical Garden",
+      technology: "NextJS | Tailwind CSS | Custom API",
+      body: "Created a performant NextJS site for a local non-profit garden. Incorporated a walking tour for blended reality.",
+      liveSite: "amygreenwell.garden",
+    },
+    {
+      img: "takethestressoff.jpg",
+      heading: "Take The Stress Off",
+      technology: "NextJS | Tailwind CSS",
+      body: "Created a performant NextJS site for a mental health professional practice.",
+      liveSite: "takethestressoff.com",
+    },
+
+    {
+      img: "konacoastplumbing.jpg",
+      heading: "Kona Coast Plumbing",
+      technology: "NextJS | Tailwind CSS",
+      body: "Created a performant NextJS site for a local family-owned plumbing business.",
+      liveSite: "konacoastplumbing.com",
+    },
+    {
+      img: "luckycathawaii.jpg",
+      heading: "Lucky Cat Boutique",
+      technology: "Shopify",
+      body: "Migrated from Squarespace and Square to Shopify and Shopify POS.",
+      liveSite: "luckycathawaii.com",
+    },
+    {
+      img: "apexhomeinspections.jpg",
+      heading: "Apex Home Inspections",
+      technology: "NextJS | Directus | Custom Express API | Tailwind CSS",
+      body: "Created a highly performant NextJS website and headless CMS for a home inspection business.",
+      liveSite: "apexinspectionshi.com",
+    },
+    {
+      img: "pc911hawaii.jpg",
+      heading: "PC911 Hawaii",
+      technology: "NextJS | Tailwind CSS",
+      body: "Created a highly performant NextJS website for a local computer repair shop.",
+      liveSite: "pc911hawaii.com",
+    },
+    {
+      img: "malamamushrooms.jpg",
+      heading: "Malama Mushrooms",
+      technology: "Liquid | Shopify",
+      body: "Updated their design to the Shopify 2.0 and optimized speed.",
+      liveSite: "malamamushrooms.com",
+    },
+    {
+      img: "francenehart.jpg",
+      heading: "Francene Hart Visionary Artist",
+      technology: "Liquid | Shopify",
+      body: "Migrated site from WordPress to Shopify resulting in boosted efficiency and conversions.",
+      liveSite: "francenehart.com",
+    },
+    {
+      img: "bigislandcleaners.jpg",
+      heading: "Big Island Cleaners",
+      technology: "WordPress",
+      body: "Created a clean performant WordPress site for a local cleaning business on the Big Island resulting in them securing multiple large cleaning clients.",
+      liveSite: "bigislandcleaners.com",
+    },
+    {
+      img: "vanceshepperson.jpg",
+      heading: "Vance Shepperson",
+      technology: "WordPress | WooCommerce",
+      body: "Built an ecommerce site to better market and sell all versions of this author's book series.",
+      liveSite: "vanceshepperson.com",
+    },
+    {
+      img: "bigislandyoga.jpg",
+      heading: "Big Island Yoga",
+      technology: "WordPress",
+      body: "Built an optimized web presence for this local non-profit yoga studio to help market and grow their membership.",
+      liveSite: "bigislandyoga.com",
+    },
+    {
+      img: "christchurchkona.jpg",
+      heading: "Christ Church Episcopal Kona",
+      technology: "WordPress",
+      body: "Built an optimized web presence for this local non-profit church and helped maintain their offince networking.",
+      liveSite: "christchurchkona.com",
+    },
+  ];
+  const parentElement = $(".portfolio-panel-items");
+  console.log("renderPortfolioItems ran: ", parentElement);
+
+  for (let i = 0; i < portfolioItems.length; i++) {
+    parentElement.append(`
+      <li class="portfolio-panel--item">
+              
+              <figure class="portfolio--img-container">
+                <img
+                  class="portfolio--img"
+                  src="assets/img/portfolio/${portfolioItems[i].img}"
+                />
+              </figure>
+              <div class="portfolio--item-details">
+                <h3>${portfolioItems[i].heading}</h3>
+                <p class="portfolio-panel-text">
+                  Technologies Used: ${portfolioItems[i].technology}
+                </p>
+                
+                <p class="portfolio-panel-text">
+                  Live Site:
+                  <a href="https://${portfolioItems[i].liveSite}" target="_blank">
+                    ${portfolioItems[i].liveSite}
+                  </a>
+                </p>
+                
+                <p class="portfolio-panel-text">
+                <br/>
+                  ${portfolioItems[i].body}
+                </p>
+                <div class="line"></div>
+              </div>
+            </li>
+      `);
+  }
+}
+
 // svg click for audio
 function audioClick() {
   let audio = new Audio("assets/audio/sailboat.mp3");
@@ -232,6 +373,7 @@ function handlePage() {
   handleViewChange();
   audioClick();
   navGrow();
+  renderPortfolioItems();
   // findPathLength();
   // addElement();
 }
